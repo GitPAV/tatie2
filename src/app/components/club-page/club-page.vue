8b8bcdb9564a0997408f6ddcd9315cbf3cbe3174
mallemouche christelle <christellema08@gmail.com>
2020-08-18 15:40:06 +0200

    wip

--- src/app/components/club-page/club-page.vue
+++ src/app/components/club-page/club-page.vue
@@ -1,5 +1,22 @@
 <template>
-    <div class="main">
-        <p>Club page</p>
+      <div class="club-page-main">
+        <HeaderClub/>
+        <InfoComponent/>  
     </div>
 </template>
+
+<script>
+    import HeaderClub from '@/app/components/club-page/components/header/header-component.vue'
+    import InfoComponent from '@/app/components/club-page/components/infos-component/infos-component.vue'
+
+    export default {
+        name: 'Header',
+        components: {
+            HeaderClub, 
+            InfoComponent
+        },
+    }
+</script>
+
+<style>
+</style>
